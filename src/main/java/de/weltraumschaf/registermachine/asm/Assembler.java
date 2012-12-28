d396a44c69a7264d882adb1db6d97ee974e3511e
Weltraumschaf <ich@weltraumschaf.de>
2012-12-28 18:28:23 +0100

    Simplify byte code generation.

--- src/main/java/de/weltraumschaf/registermachine/asm/Assembler.java
+++ src/main/java/de/weltraumschaf/registermachine/asm/Assembler.java
@@ -88,35 +88,15 @@ public class Assembler {
         final List<String> args = parts.subList(1, parts.size());
         final OpCode bc = OpCode.lokup(parts.get(0));
 
-        switch (bc) {
-            case MOVE:
-                generateMoveCode(bc, bytecode, args);
-                break;
-            case LOADC:
-                generateLoadConstantCode(bc, bytecode, args);
-                break;
-            case ADD:
-                generateAddCode(bc, bytecode, args);
-                break;
-            case SUB:
-                generateSubCode(bc, bytecode, args);
-                break;
-            case MUL:
-                generateMullCode(bc, bytecode, args);
-                break;
-            case DIV:
-                generateDivCode(bc, bytecode, args);
-                break;
-            case PRINT:
-                generatePrintCode(bc, bytecode, args);
-                break;
-            case PRINTLN:
-                generatePrintLnCode(bc, bytecode, args);
-                break;
-            case UNKWONN:
-            default:
-                throw new AssemblerSyntaxException(String.format("Unknown mnemonic '%s'!", parts.get(0)));
+        if (bc == OpCode.UNKWONN) {
+            throw new AssemblerSyntaxException(String.format("Unknown mnemonic '%s'!", parts.get(0)));
+        } else {
+            assertArgCount(args, bc);
+            bytecode.add(bc.getCode());
 
+            for (int i = 0; i < args.size(); ++i) {
+                bytecode.addAll(createByteListFromNumericArg(args.get(i)));
+            }
         }
     }
 
@@ -131,68 +111,11 @@ public class Assembler {
         final byte[] bytes = ByteInt.bytesFromInt(value);
         return Arrays.asList(ByteArray.toObject(bytes));
     }
-    private void generateMoveCode(final OpCode bc, final List<Byte> bytecode, final List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-    }
-
-    private void generateAddCode(final OpCode bc, final List<Byte> bytecode, final List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(2)));
-    }
-
-    private void generateSubCode(final OpCode bc, final List<Byte> bytecode, final List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(2)));
-    }
-
-    private void generateMullCode(final OpCode bc, final List<Byte> bytecode, final List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(2)));
-    }
-
-    private void generateDivCode(final OpCode bc, final List<Byte> bytecode, final List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(2)));
-    }
 
     private void assertArgCount(final List<String> args, final OpCode bc) throws AssemblerSyntaxException {
         if (args.size() != bc.getArgCount().getCount()) {
             throw new AssemblerSyntaxException(String.format(ARG_CNT_ERR_FMT, bc.name(), bc.getArgCount().getCount()));
         }
-    }
-
-    private void generateLoadConstantCode(OpCode bc, List<Byte> bytecode, List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-        bytecode.addAll(createByteListFromNumericArg(args.get(1)));
-    }
-
-    private void generatePrintCode(OpCode bc, List<Byte> bytecode, List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
-    }
-
-    private void generatePrintLnCode(OpCode bc, List<Byte> bytecode, List<String> args) throws AssemblerSyntaxException {
-        assertArgCount(args, bc);
-        bytecode.add(bc.getCode());
-        bytecode.addAll(createByteListFromNumericArg(args.get(0)));
     }
 
 }
