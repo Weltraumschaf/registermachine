1f87e360c616a512659d06d5bc3a150944d5bf02
Weltraumschaf <ich@weltraumschaf.de>
2012-12-30 16:59:25 +0100

    Add api doc.

--- src/main/java/de/weltraumschaf/registermachine/asm/CharStream.java
+++ src/main/java/de/weltraumschaf/registermachine/asm/CharStream.java
@@ -12,19 +12,42 @@
 package de.weltraumschaf.registermachine.asm;
 
 /**
+ * Represents a string as stream accessible character by character.
  *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 class CharStream {
 
+    /**
+     * Line number in the source.
+     *
+     * By default this may be -1.
+     */
     private final int lineNumber;
+    /**
+     * String representing the line.
+     */
     private final String str;
+    /**
+     * Current character position.
+     */
     private int index;
 
+    /**
+     * Initializes {@link #lineNumber} with -1.
+     *
+     * @param str representing the line
+     */
     CharStream(final String str) {
         this(str, -1);
     }
 
+    /**
+     * Dedicated constructor.
+     * 
+     * @param str representing the line
+     * @param lineNumber line number in the source string
+     */
     CharStream(final String str, final int lineNumber) {
         super();
         this.str = str;
